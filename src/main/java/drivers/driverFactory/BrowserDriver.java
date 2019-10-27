6f946d3c1c2c380dfd1d9356dd6fdf160f1ff37e
Mayank Chauhan <mayank.testvagrant@embibe.com>
2019-10-27 09:31:37 +0530

    framework structure created

---
+++ src/main/java/drivers/driverFactory/BrowserDriver.java
@@ -0,0 +1,10 @@
+package drivers.driverFactory;
+
+import org.openqa.selenium.WebDriver;
+
+public interface BrowserDriver {
+    String driverEnvironment = System.getProperty("driverEnvironment");
+    String driver = System.getProperty("driver");
+    WebDriver getDriver() throws Exception;
+    void setDriverBinaryPath();
+}
